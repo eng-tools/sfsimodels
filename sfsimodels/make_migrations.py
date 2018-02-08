a6bf8ef445635f752340913ad7dcabcc2a8b0130
millen1m <maxim.millen@gmail.com>
2018-02-08 14:38:50 +0000

    added instructions for building a migration tool.

---
+++ sfsimodels/make_migrations.py
@@ -0,0 +1,6 @@
+# TODO: implement a method of storing model states,
+# For each version, cycle through the parameters and store a full yaml file and then
+# compare the new yaml file with the previous version and detect changes and write them to a migration script.
+# To upgrade, type `python sfsi_migrate <file-name> <old-version> <target-version>`
+# The migrator then with run all migration scripts between the new and the old version.
+
