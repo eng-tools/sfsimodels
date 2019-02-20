73f976f5e4c1438abc6c102b0280d919843d19b2
millen1m <maxim.millen@gmail.com>
2019-02-20 21:59:45 +1300

    fixed a deprecation error.

--- sfsimodels/files.py
+++ sfsimodels/files.py
@@ -109,7 +109,7 @@ def ecp_dict_to_objects(ecp_dict, custom_map=None, verbose=0):
         "soil-soil_stress_dependent": soils.StressDependentSoil,
         "soil_profile-soil_profile": soils.SoilProfile,
         "building-building": buildings.Building,
-        "building-frame_building": buildings.BuildingFrame,
+        "building-frame_building": buildings.FrameBuilding,
         "building-frame_building2D": buildings.FrameBuilding2D,
         "building-building_frame2D": buildings.FrameBuilding2D,  # deprecated type
         "building-wall_building": buildings.WallBuilding,
