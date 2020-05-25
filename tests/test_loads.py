4981f7adb3e245d5ba2813d9c85f7f9e365b11e9
millen1m <maxim.millen@gmail.com>
2020-05-26 08:32:41 +1200

    Fixed broken test.

--- tests/test_loads.py
+++ tests/test_loads.py
@@ -2,7 +2,7 @@ from sfsimodels.models import loads
 
 
 def test_loads_add_and_remove():
-    load = loads.LoadOnAxes(p_x=10)
+    load = loads.Load(p_x=10)
     assert load.p_x == 10
     assert load.p_y is None
     load.p_x = 15
@@ -17,3 +17,22 @@ def test_loads_add_and_remove():
     assert load.t_xx == 4
     assert load.t_yy == 5
     assert load.t_zz == 6
+
+
+def test_load_at_coords_add_and_remove():
+    load = loads.LoadAtCoords(p_x=10, x=3)
+    assert load.p_x == 10
+    assert load.p_y is None
+    load.p_x = 15
+    load.p_y = 9
+    assert load.p_x == 15
+    assert load.p_y == 9
+    load.p_z = 3
+    load.t_xx = 4
+    load.t_yy = 5
+    load.t_zz = 6
+    assert load.p_z == 3
+    assert load.t_xx == 4
+    assert load.t_yy == 5
+    assert load.t_zz == 6
+    assert load.x == 3
