4771f7693c7ed30c05e82e41401c7d141e02dcf9
millen1m <maxim.millen@gmail.com>
2021-10-18 12:45:32 +1300

    v0.9.40 Fixed test.

--- sfsimodels/__about__.py
+++ sfsimodels/__about__.py
@@ -1,4 +1,4 @@
 __project__ = "sfsimodels"
 __author__ = "Maxim Millen"
-__version__ = "0.9.39"
+__version__ = "0.9.40"
 __license__ = "MIT"
--- tests/test_concrete.py
+++ tests/test_concrete.py
@@ -1,10 +1,11 @@
 import numpy as np
-from sfsimodels.models.materials import ReinforcedConcreteMaterial
+from sfsimodels.models.materials import ReinforcedConcreteMaterial, calc_e_mod_conc_via_unknown
 
 
 def test_youngs():
     concrete = ReinforcedConcreteMaterial()
-    assert np.isclose(concrete.e_mod_conc, 25084388909.2, rtol=0.0001)
+    e_mod_conc = calc_e_mod_conc_via_unknown(concrete.fc)
+    assert np.isclose(e_mod_conc, 25084388909.2, rtol=0.0001)
 
 
 def can_iterate():
